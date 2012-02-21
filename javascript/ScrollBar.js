0e3f3f1524843df4cb283ad9d0c2507adc278e7b
André Fiedler <kontakt@visualdrugs.net>
2012-02-21 20:34:46 +0000

    "ScrollBar Update"

--- javascript/ScrollBar.js
+++ javascript/ScrollBar.js
@@ -1,107 +1,126 @@
-/*
----
- 
-name: Scrollbar
-
-description: A simple Apple-style productbrowser, that extends Slider using a container.
-
-version: 2.0.0-wip
-
-copyright: Enrique Erne (http://mild.ch/)
-
-license: MIT License
-
-authors:
-- Enrique Erne
-
-requires: [Core/Class, Core/Element.Event, Core/Element.Dimensions, Core/Fx.Tween, Core/Fx.Transitions, Core/Selectors, More/Slider]
-
-provides: ScrollBar
- 
-...
-*/
-
 var ScrollBar = new Class({
-    
-	Extends: Slider,
+
+    Implements: [Events, Options],
 
 	options: {
-		onTick: function(pos){
-			if (this.options.snap) pos = this.toPosition(this.step);
-			if (this.knobFx) this.knob.tween(this.property, pos);
-			else this.knob.setStyle(this.property, pos);
-		},
-		onChange: function(step){
-			this.scroll(step / this.ratioScroll);
-		},
-		/*onComplete: function(step){},*/
-		initialStep: 0,
-		snap: false,
-		offset: 0,
-		range: false,
-		wheel: true,
-		steps: 100,
-		mode: 'horizontal',
-		scroll: {
-			// onStart: function(){},
-			// onComplete: function(){},
-			duration: 200,
-			link: 'cancel',
-			transition: 'linear'
-		},
-		knob: {
-			duration: 200,
-			transition: 'linear',
-			link: 'cancel'
-		}
+		maxThumbSize: 15,
+		wheel: 8
 	},
 
-	initialize: function(scroller, slider, knob, options){
+	initialize: function(main, content, scrollbar, options){
 		this.setOptions(options);
-		this.container = document.id(scroller);
-        
-		this.scroller = this.container.getFirst();
-		this.parent(slider, knob);
 		
-		this.scroller.set('tween', this.options.scroll);
-		this.knob.set('tween', this.options.knob);
+		this.main = $(main);
+		this.content = $(content);				
+
+		this.vScrollbar = $(scrollbar);					
+
+		this.vTrack = new Element('div', {
+			'class': 'vTrack'
+		}).injectInside(this.vScrollbar);
+			
+		this.vThumb = new Element('div', {
+			'class': 'vThumb'
+		}).injectInside(this.vTrack);		
 		
-		this.scrollFx = this.scroller.get('tween');
-		if (this.options.knob.duration || this.options.knob.transition) this.knobFx = this.knob.get('tween');
+		this.bound = {
+			'vStart': this.vStart.bind(this),				
+			'end': this.end.bind(this),
+			'vDrag': this.vDrag.bind(this),			
+			'wheel': this.wheel.bind(this),
+			'vPage': this.vPage.bind(this)			
+		};
+
+		this.vPosition = {};			
+		this.vMouse = {};		
+		this.update();
+		this.attach();
+	},
+
+	update: function(){
 		
-		if (this.options.wheel) this.container.addEvent('mousewheel', function(event){
-			this.element.fireEvent('mousewheel', event);
-		}.bind(this));
+		this.main.setStyle('height', this.content.offsetHeight + this.hScrollOffset);
+		this.vTrack.setStyle('height', this.content.offsetHeight);
+					
+		this.main.setStyle('width', this.content.offsetWidth + 15);
 		
-		this.ratio = this.steps / (this.element.getSize()[this.axis] - this.knob.getSize()[this.axis]);
-		this.ratioScroll = this.steps / (this.scroller.getSize()[this.axis] - this.container.getSize()[this.axis]);
+		// Remove and replace vertical scrollbar			
+		if (this.content.scrollHeight <= this.main.offsetHeight) {
+			this.vScrollbar.setStyle('display', 'none');	
+			this.content.setStyle('width', this.content.offsetWidth + 15);	
+		} else {
+			this.vScrollbar.setStyle('display', 'block');			
+		}
+		
+		this.vContentSize = this.content.offsetHeight;
+		this.vContentScrollSize = this.content.scrollHeight;
+		this.vTrackSize = this.vTrack.offsetHeight;
+
+		this.vContentRatio = this.vContentSize / this.vContentScrollSize;
+
+		this.vThumbSize = (this.vTrackSize * this.vContentRatio).limit(this.options.maxThumbSize, this.vTrackSize) - 2;
+
+		this.vScrollRatio = this.vContentScrollSize / this.vTrackSize;
+
+		this.vThumb.setStyle('height', this.vThumbSize);
+
+		this.vUpdateThumbFromContentScroll();
+		this.vUpdateContentFromThumbPosition();
+		
+	},
+
+	vUpdateContentFromThumbPosition: function(){
+		this.content.scrollTop = this.vPosition.now * this.vScrollRatio;
+	},	
+
+	vUpdateThumbFromContentScroll: function(){
+		this.vPosition.now = (this.content.scrollTop / this.vScrollRatio).limit(0, (this.vTrackSize - this.vThumbSize)) + 1;
+		this.vThumb.setStyle('top', this.vPosition.now);
+	},		
+
+	attach: function(){
+		this.vThumb.addEvent('mousedown', this.bound.vStart);
+		if (this.options.wheel) this.content.addEvent('mousewheel', this.bound.wheel);
+		this.vTrack.addEvent('mouseup', this.bound.vPage);					
 	},
 	
-	scroll: function(pos){
-		this.scroller.tween(this.property, -pos);
+	wheel: function(event){
+		this.content.scrollTop -= event.wheel * this.options.wheel;
+		this.vUpdateThumbFromContentScroll();
+		event.stop();
 	},
-	
-	draggedKnob: function(){
-		this.parent();
-		this.scrollFx.cancel();
-		this.scroller.setStyle(this.property, -this.step / this.ratioScroll);
-	},
-	
-	clickedElement: function(event){
-		if (this.knobFx && event.target === this.knob) this.knobFx.cancel();
-		this.parent(event);
-	},
-	
-	scrolledElement: function(event){
-		if (this.knobFx) this.knobFx.cancel();
-		var mode = (this.options.mode == 'horizontal') ? (event.wheel < 0) : (event.wheel > 0);
-		this.set(this.step + 10 * ((mode) ? -this.stepSize : this.stepSize));
+
+	vPage: function(event){
+		if (event.page.y > this.vThumb.getPosition().y) this.content.scrollTop += this.content.offsetHeight;
+		else this.content.scrollTop -= this.content.offsetHeight;
+		this.vUpdateThumbFromContentScroll();
+		event.stop();
+	},	
+
+	vStart: function(event){
+		this.vScrollbar.addClass('active');
+		this.vMouse.start = event.page.y;
+		this.vPosition.start = this.vThumb.getStyle('top').toInt();
+		document.addEvent('mousemove', this.bound.vDrag);
+		document.addEvent('mouseup', this.bound.end);
+		this.vThumb.addEvent('mouseup', this.bound.end);
 		event.stop();
 	},
 	
-	set: function(pos){
-		if (typeOf(pos) === 'element') pos = pos.getPosition(this.scroller)[this.axis] * this.ratioScroll;
-		this.parent(pos);
-	}
+	end: function(event){
+		this.vScrollbar.removeClass('active');
+		document.removeEvent('mousemove', this.bound.vDrag);
+		document.removeEvent('mousemove', this.bound.hDrag);			
+		document.removeEvent('mouseup', this.bound.end);
+		this.vThumb.removeEvent('mouseup', this.bound.end);		
+		event.stop();
+	},
 
+	vDrag: function(event){
+		this.vMouse.now = event.page.y;
+		this.vPosition.now = (this.vPosition.start + (this.vMouse.now - this.vMouse.start)).limit(0, (this.vTrackSize - this.vThumbSize));
+		this.vUpdateContentFromThumbPosition();
+		this.vUpdateThumbFromContentScroll();
+		event.stop();
+	}
 });
